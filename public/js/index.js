47cd628818026b499c90033e3d367cd976c9875f
Danny Sun <danny.sun@companiondxlab.com>
2016-11-17 15:49:16 -0600

    added user login capabilities

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,9 @@
 var socket = io();
+
+
+
+
+
 socket.on('connect', function() {
   console.log('connected to server');
 })
@@ -7,16 +12,27 @@ socket.on('disconnect', function() {
   console.log('disconnected from server');
 })
 
+socket.on('user-list-refresh', function(users){
+  var html = '';
+  users.forEach(function(user){
+    html += `<div class="chat-member"><div class="img-container"><a href="#"><img class="mg-responsive" src="img/ui-254/2.jpg" alt=""></a><span class="st-live status"></span></div><h4><a href="#">${user}</a></h4><p>I am online now.</p><div class="clearfix"></div></div>`
+    console.log('added user', user);
+  })
+  $('.chat-contact').html(html)
+})
+
 // listening FROM server for new message
 socket.on('newMessage', function(obj) {
-  var div = jQuery(`<div class='chat-message left'><i class="fa fa-user-circle fa-3 message-avatar" aria-hidden="true"></i><div class='message'><a class='message-author'>${obj.from}</a><span class='message-date'>${obj.createdAt}</span><span class='message-content'>${obj.text}</span></div></div>`)
-  jQuery('.chat-discussion').append(div)
+  // var div = jQuery(`<div class='chat-message left'><i class="fa fa-user-circle fa-3 message-avatar" aria-hidden="true"></i><div class='message'><a class='message-author'>${obj.from}</a><span class='message-date'>${obj.createdAt}</span><span class='message-content'>${obj.text}</span></div></div>`)
+  var div = jQuery(`<div class="chat-box chat-in"><div class="img-container"><img class="img-responsive" src="img/ui-254/4.jpg" alt=""></div><div class="message"><h5>${obj.from.toUpperCase()}&nbsp;&nbsp;<i class="fa fa-clock-o"></i>&nbsp; ${obj.createdAt}</h5><p>${obj.text}</p></div><div class="clearfix"></div></div>`)
+  jQuery('.chat-content').append(div)
+  $(".chat-content").scrollTop($(".chat-content").prop("scrollHeight"));
 })
 
 socket.on('newLocationMessage', function(obj) {
   console.log(obj)
   var div = jQuery(`<div class='chat-message left'><i class="fa fa-user-circle fa-3 message-avatar" aria-hidden="true"></i><div class='message'><a class='message-author'>${obj.from}</a><span class='message-date'>${obj.createdAt}</span><span class='message-content'><a href='${obj.url}'>Current Location</a></span></div></div>`)
-  jQuery('.chat-discussion').append(div)
+  jQuery('.chat-content').append(div)
 })
 
 // form handling
@@ -29,6 +45,14 @@ jQuery('#message-form').on('submit', function(e){
   function(){
   });
   jQuery('[name=message-field]').val('')
+});
+
+jQuery('#user-form').on('submit', function(e){
+  e.preventDefault();
+  socket.emit('new-user', { user: $('#username').val() })
+  $('#username').val('')
+  $('.ui-254').show()
+  $('#userLogin').hide()
 });
 
 var locationButton = $('#send-location');
@@ -49,3 +73,29 @@ locationButton.on('click', function(e){
   })
 
 })
+
+$(".chat-contact").niceScroll({
+  cursorcolor:"#999",
+  cursoropacitymin:0,
+  cursoropacitymax:0.3,
+  cursorwidth:5,
+  cursorborder:"0px",
+  cursorborderradius:"0px",
+  cursorminheight:50,
+  zindex:1,
+  mousescrollstep:20,
+ nativeparentscrolling: true
+});
+
+$(".chat-content").niceScroll({
+  cursorcolor:"#999",
+  cursoropacitymin:0,
+  cursoropacitymax:0.3,
+  cursorwidth:5,
+  cursorborder:"0px",
+  cursorborderradius:"0px",
+  cursorminheight:50,
+  zindex:1,
+  mousescrollstep:20,
+  nativeparentscrolling: true
+});
--- server/server.js
+++ server/server.js
@@ -3,13 +3,14 @@ const http = require('http');
 const express = require('express');
 const publicPath = path.join(__dirname, '../public');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3002;
 
 var server = http.createServer(app);
 const socketIO = require('socket.io');
 var io = socketIO(server)
 
 const { generateMessage, generateLocationMessage } = require('./utils/message');
+var users = [];
 
 app.use(express.static(publicPath));
 app.get('/', (req, res) => {
@@ -25,14 +26,20 @@ io.on('connection', (socket) => {
   // socket welcomes the user
   socket.emit('newMessage', generateMessage('admin', 'Welcome to the Chat!'))
 
-  // socket informs everyone else but the new client that a user has joined
-  socket.broadcast.emit('newMessage', generateMessage('admin', 'A New User has joined the Chat!'))
+  socket.on('new-user', (data) => {
+    // socket informs everyone else but the new client that a user has joined
+    socket.broadcast.emit('newMessage', generateMessage('admin', 'A New User has joined the Chat!'))
+    socket.username = data.user;
+    users.push(socket.username)
+    updateUsers(users)
+  })
 
+  function updateUsers(users){
+    io.emit('user-list-refresh', users)
+  }
 
   // listen for new messages
   socket.on('createMessage', (message, cb) => {
-    console.log('createMessage', message)
-
     // emit to everyone
     io.emit('newMessage', generateMessage(message.from, message.text))
     cb('this from the server');
@@ -43,7 +50,9 @@ io.on('connection', (socket) => {
   });
 
   socket.on('disconnect', () => {
-    console.log('client disconnected');
+    users.splice(users.indexOf(socket.username, 1))
+    io.emit('user-list-refresh', users)
+    console.log(socket.username, 'has disconnected');
   })
 
 })
