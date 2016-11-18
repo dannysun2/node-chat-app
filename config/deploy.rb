e7b2431673eed4b206321d0c1f14df1cca4484eb
Danny Sun <danny.sun@companiondxlab.com>
2016-11-18 01:31:22 -0600

    deploying'

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,43 +1,13 @@
-# config valid only for current version of Capistrano
-lock '3.6.1'
-set :stages, ["production"]
-set :application, 'nodechat-socketio'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'nodechat'
+set :repo_url, 'git@github.com:dannysun2/node-chat-app.git'
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-
-# Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, "/home/deploy/apps/#{fetch(:application)}"
+set :keep_releases,   5
 
-# Default value for :scm is :git
-# set :scm, :git
 set :ssh_options, {
     forward_agent: true,
     port: 22
 }
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
-
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# append :linked_files, 'config/database.yml', 'config/secrets.yml'
-
-# Default value for linked_dirs is []
-# append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-set :keep_releases, 5
-
 namespace :deploy do
 
   desc 'Restart application'
